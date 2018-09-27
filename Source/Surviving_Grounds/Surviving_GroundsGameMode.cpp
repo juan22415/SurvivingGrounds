710b83b7f6f9de9731042b04ff654d749c7138f1
Juan camilo rios <juan22415@hotmail.com>
2018-09-27 17:13:26 -0500

    Procedural levels & AI Fixing

--- Source/Surviving_Grounds/Surviving_GroundsGameMode.cpp
+++ Source/Surviving_Grounds/Surviving_GroundsGameMode.cpp
@@ -9,7 +9,7 @@ ASurviving_GroundsGameMode::ASurviving_GroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Weapons/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
